7477211f77d7ed3cd398e1d61c093564d4fb793f
TheEpicBlock <git.teb@theepicblock.nl>
2021-09-10 17:39:07 +0200

    Initiate repo

---
+++ src/main/java/nl/theepicblock/resourcelocatorapi/ResourceLocatorApi.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.resourcelocatorapi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ResourceLocatorApi implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
