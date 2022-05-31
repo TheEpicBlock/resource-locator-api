867bb285ad41f3be1912780c0dc8e880f865d607
TheEpicBlock <git.teb@theepicblock.nl>
2022-05-31 16:29:02 +0200

    Arrp support

--- src/main/java/nl/theepicblock/resourcelocatorapi/ResourceLocatorApi.java
+++ src/main/java/nl/theepicblock/resourcelocatorapi/ResourceLocatorApi.java
@@ -3,6 +3,7 @@ package nl.theepicblock.resourcelocatorapi;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resource.ResourceType;
 import nl.theepicblock.resourcelocatorapi.api.AssetContainer;
+import nl.theepicblock.resourcelocatorapi.impl.ArrpProvider;
 import nl.theepicblock.resourcelocatorapi.impl.CompositeResourcePack;
 import nl.theepicblock.resourcelocatorapi.impl.FapiProvider;
 import nl.theepicblock.resourcelocatorapi.impl.QslProvider;
@@ -14,11 +15,18 @@ public class ResourceLocatorApi {
 		// Create empty composite pack
 		var compositePack = new CompositeResourcePack(ResourceType.CLIENT_RESOURCES);
 
+		if (loader.isModLoaded("advanced_runtime_resource_pack")) {
+			ArrpProvider.addPacksBeforeVanilla(compositePack);
+		}
+
 		if (loader.isModLoaded("quilt_resource_loader")) {
-			System.out.println("T");
 			QslProvider.addPacks(compositePack);
 		} else {
 			FapiProvider.addPacks(compositePack);
+		}
+
+		if (loader.isModLoaded("advanced_runtime_resource_pack")) {
+			ArrpProvider.addPacksAfterVanilla(compositePack);
 		}
 
 		return compositePack;
---
+++ src/main/java/nl/theepicblock/resourcelocatorapi/impl/ArrpProvider.java
@@ -0,0 +1,21 @@
+package nl.theepicblock.resourcelocatorapi.impl;
+
+import net.devtech.arrp.api.RRPCallback;
+import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
+import net.minecraft.resource.ResourcePack;
+
+import java.util.ArrayList;
+
+public class ArrpProvider {
+    public static void addPacksBeforeVanilla(CompositeResourcePack pack) {
+        var l = new ArrayList<ResourcePack>();
+        RRPCallback.BEFORE_VANILLA.invoker().insert(l);
+        l.forEach(pack::append);
+    }
+
+    public static void addPacksAfterVanilla(CompositeResourcePack pack) {
+        var l = new ArrayList<ResourcePack>();
+        RRPCallback.AFTER_VANILLA.invoker().insert(l);
+        l.forEach(pack::append);
+    }
+}
