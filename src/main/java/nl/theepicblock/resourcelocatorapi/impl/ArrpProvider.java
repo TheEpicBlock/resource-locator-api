141a1a9112fe41ffb09e32996add2253b83188e0
TheEpicBlock <git.teb@theepicblock.nl>
2023-02-21 13:42:07 +0100

    Remove ARRP

--- src/main/java/nl/theepicblock/resourcelocatorapi/impl/ArrpProvider.java
+++ src/main/java/nl/theepicblock/resourcelocatorapi/impl/ArrpProvider.java
@@ -1,6 +1,6 @@
 package nl.theepicblock.resourcelocatorapi.impl;
 
-import net.devtech.arrp.api.RRPCallback;
+// import net.devtech.arrp.api.RRPCallback;
 import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
 import net.minecraft.resource.ResourcePack;
 
@@ -8,14 +8,14 @@ import java.util.ArrayList;
 
 public class ArrpProvider {
     public static void addPacksBeforeVanilla(CompositeResourcePack pack) {
-        var l = new ArrayList<ResourcePack>();
-        RRPCallback.BEFORE_VANILLA.invoker().insert(l);
-        l.forEach(pack::append);
+        // var l = new ArrayList<ResourcePack>();
+        // RRPCallback.BEFORE_VANILLA.invoker().insert(l);
+        // l.forEach(pack::append);
     }
 
     public static void addPacksAfterVanilla(CompositeResourcePack pack) {
-        var l = new ArrayList<ResourcePack>();
-        RRPCallback.AFTER_VANILLA.invoker().insert(l);
-        l.forEach(pack::append);
+        // var l = new ArrayList<ResourcePack>();
+        // RRPCallback.AFTER_VANILLA.invoker().insert(l);
+        // l.forEach(pack::append);
     }
 }
